59c3ffa5a3e5b0d5e5237d1aa62ab51c9f3e72cc
Nevathan <nevathan321@gmail.com>
2026-01-01 22:39:09 -0500

    Added another option
    Used a join instead of a subquery this time

--- LEETCODE_1789.sql
+++ LEETCODE_1789.sql
@@ -1,6 +1,10 @@
-Select employee_id, department_id
-from Employee
-where primary_flag = 'Y'
-    or employee_id in (Select employee_id from Employee 
-                        group by employee_id
-                        having count(department_id) = 1)
+SELECT employee_id, department_id
+FROM Employee
+WHERE primary_flag = 'Y'
+
+UNION
+
+SELECT employee_id, department_id
+FROM Employee
+GROUP BY employee_id
+HAVING COUNT(*) = 1;
