22d69ca5359e33be74491678efe6498d0eb2be0f
nevathan321 <150630749+nevathan321@users.noreply.github.com>
2026-01-06 21:29:50 -0500

    LEETCODE 1978, two solutions

--- SQL_leetcode/LEETCODE_1978.sql
+++ SQL_leetcode/LEETCODE_1978.sql
@@ -1,7 +1,23 @@
-SELECT employee_id
-FROM Employees
-WHERE salary < 30000
-  AND manager_id NOT IN (
-    SELECT employee_id
-    FROM Employees
-  );
+
+/* Solution 2 but more optimized */
+/* Using LEFT JOIN to find employees without managers directly, form of self - join */
+
+SELECT e1.employee_id
+FROM Employees e1
+LEFT JOIN Employees e2
+ON e1.manager_id = e2.employee_id
+WHERE e1.salary < 30000 AND e2.employee_id IS NULL AND e1.manager_id IS NOT NULL
+ORDER BY employee_id; 
+
+
+/* Solution 1 I want to find the employee who has a manager_id that is not in the employee id column */
+
+
+Select 
+    employee_id 
+From Employees 
+Where manager_id not in (
+    Select employee_id 
+    From Employees 
+) AND salary < 30000 
+Order by employee_id ASC 
