2334e0109db1b4c00680d215096b90bd3d11cd65
nevathan321 <150630749+nevathan321@users.noreply.github.com>
2026-01-26 02:03:03 -0500

    Sql leetcode medium

---
+++ SQL_Leetcode/SQL_LEETCODE_585.sql
@@ -0,0 +1,8 @@
+SELECT ROUND(SUM(tiv_2016), 2) as tiv_2016
+FROM Insurance
+WHERE tiv_2015 IN (
+    SELECT tiv_2015
+    FROM Insurance
+    GROUP BY tiv_2015
+    HAVING COUNT(*) > 1
+) AND (lat, lon) in (Select lat, lon From insurance Group by lat, lon Having Count(*) = 1) 
