d820a660410b56514ce10b824e220ad5758ef1ab
nevathan321 <150630749+nevathan321@users.noreply.github.com>
2026-02-02 19:59:17 -0500

    redid SQL leetcode question

--- SQL_Leetcode/SQL_LEETCODE_1211.sql
+++ SQL_Leetcode/SQL_LEETCODE_1211.sql
@@ -1,7 +1,9 @@
+-- Redone the following SQL query to calculate the quality and poor query percentage for each query name in the Queries table.
 SELECT
   query_name,
   ROUND( SUM(1.0 * rating / position) / COUNT(*), 2) AS quality,
-  ROUND( SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 2) AS poor_query_percentage
+  ROUND( SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 2) AS
+poor_query_percentage
 FROM Queries
 WHERE query_name IS NOT NULL
 GROUP BY query_name;
